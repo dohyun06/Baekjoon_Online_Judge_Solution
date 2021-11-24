0b09eaa5e7ee530f08822906dd616be8df35330b
dohyun06 <hyun4you2006@naver.com>
2021-11-25 07:39:15 +0900

    Create BOJ 1302.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1302.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+#include <map>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N, index = 0;
+	std::string input;
+	std::map<std::string, int> m;
+
+	std::cin >> N;
+
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> input;
+
+		m[input]++;
+	}
+
+	for (auto i : m) index = std::max(index, i.second);
+	for (auto i : m)
+		if (i.second == index) {
+			std::cout << i.first; 
+			break;
+		}
+
+	return 0;
+}
