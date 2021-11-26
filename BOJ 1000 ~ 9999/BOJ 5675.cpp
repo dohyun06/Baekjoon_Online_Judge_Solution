77239fae3d09311037e6c5b976098d1ee9a988d0
dohyun06 <hyun4you2006@naver.com>
2021-11-26 20:53:02 +0900

    Create BOJ 5675.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 5675.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int A;
+
+	while (std::cin >> A)
+		std::cout << (A % 6 ? 'N' : 'Y') << '\n';
+
+	return 0;
+}
