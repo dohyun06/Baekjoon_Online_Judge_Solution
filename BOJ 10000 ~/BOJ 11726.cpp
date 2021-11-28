0bfc415340b6174039dc94c65aea71ab823c41b1
dohyun06 <hyun4you2006@naver.com>
2021-11-28 09:52:58 +0900

    Create BOJ 11726.cpp

---
+++ BOJ 10000 ~/BOJ 11726.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main(void)
+{
+    std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+    int n, dp[1001] = { 1, 1, 2 };
+
+    std::cin >> n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] %= 10007;
+    }
+
+    std::cout << dp[n];
+
+    return 0;
+}
