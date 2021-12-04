0df97121dafd2587ec4b6cea5541c1ae5c1da4fc
dohyun06 <hyun4you2006@naver.com>
2021-12-05 08:41:43 +0900

    Create BOJ 21598.cpp

---
+++ BOJ 10000 ~/BOJ 21598.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int a;
+
+	std::cin >> a;
+
+	while (a--) std::cout << "SciComLove" << '\n';
+
+	return 0;
+}
