47eb3dc25de70a5acf34f873203dee8ad95c75d2
dohyun06 <hyun4you2006@naver.com>
2021-12-12 00:43:06 +0900

    Create BOJ 23813.cpp

---
+++ BOJ 10000 ~/BOJ 23813.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	long long N, len, sum = 0;
+	std::string s;
+
+	std::cin >> s;
+
+	len = s.length();
+	N = std::stoi(s);
+
+	for (int i = 0; i < len; i++) {
+		sum += N;
+		N = N / 10 + (N % 10) * std::pow(10, len - 1);
+	}
+
+	std::cout << sum;
+
+	return 0;
+}
