cb1f1f55515fbffc1ba80b3f4cb428222675911a
dohyun06 <hyun4you2006@naver.com>
2021-12-12 08:44:08 +0900

    Create BOJ 1003.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1003.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main(void) {
+	int T, N, fi[40] = { 0,1, };
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> N;
+		if (N == 0) cout << "1 0" << endl;
+		else {
+			for (int j = 1; j < N; j++) {
+				fi[j + 1] = fi[j - 1] + fi[j];
+			}
+			cout << fi[N - 1] << " " << fi[N] << endl;
+		}
+	}
+	return 0;
+}
