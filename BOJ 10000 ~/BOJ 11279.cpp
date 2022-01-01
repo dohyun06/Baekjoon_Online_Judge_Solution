efa1a41b28372e22e1a84bf5ba8085a98b22b5a0
dohyun06 <hyun4you2006@naver.com>
2022-01-01 15:39:49 +0900

    Create BOJ 11279.cpp

---
+++ BOJ 10000 ~/BOJ 11279.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <queue>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N, input;
+	std::priority_queue<int, std::vector<int>, std::less<int>> pq;
+
+	std::cin >> N;
+
+	while (N--) {
+		std::cin >> input;
+		if (input) pq.push(input);
+		else {
+			if (pq.empty()) std::cout << 0 << '\n';
+			else {
+				std::cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+	}
+
+	return 0;
+}
