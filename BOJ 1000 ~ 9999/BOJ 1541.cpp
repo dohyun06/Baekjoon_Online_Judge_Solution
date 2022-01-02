0f807c09fef57a1e3c67afb23e19a1a3cb019418
dohyun06 <hyun4you2006@naver.com>
2022-01-02 14:31:27 +0900

    Create BOJ 1541.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1541.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int num=0, sum=0, flag=1;
+	std::string s;
+
+	std::cin >> s;
+
+	for (char c : s) {
+		if (c == '+') {
+			sum += num;
+			num = 0;
+		}
+		else if (c == '-') {
+			flag = -1;
+			sum += num;
+			num = 0;
+		}
+		else {
+			num = num * 10 + (c - '0') * flag;
+		}
+	}
+	std::cout << sum + num;
+
+	return 0;
+}
