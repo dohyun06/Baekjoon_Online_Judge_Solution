5fc84a0c5e0e720d5efb86bf979f8e37c17d7a19
dohyun06 <hyun4you2006@naver.com>
2022-01-02 14:07:10 +0900

    Create BOJ 9375.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 9375.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int T, n, sum;
+	std::string s1, s2;
+
+	std::cin >> T;
+
+	while (T--) {
+		std::cin >> n;
+
+		sum = 1;
+		std::vector<std::string> v;
+		std::map<std::string, int> m;
+
+		while (n--) {
+			std::cin >> s1 >> s2;
+			m[s2]++;
+			if (std::find(v.begin(), v.end(), s2) == v.end())
+				v.push_back(s2);
+		}
+		for (int i = 0; i < v.size(); i++) {
+			sum *= m[v[i]] + 1;
+		}
+		std::cout << sum - 1 << '\n';
+	}
+
+	return 0;
+}
