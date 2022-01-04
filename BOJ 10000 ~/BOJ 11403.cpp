9fba0c9271c7e4973b9fc8310a46229e9b756ade
dohyun06 <hyun4you2006@naver.com>
2022-01-04 22:59:06 +0900

    Create BOJ 11403.cpp

---
+++ BOJ 10000 ~/BOJ 11403.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N;
+
+	std::cin >> N;
+
+	std::vector<std::vector<int>> graph(N, std::vector<int>(N));
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			std::cin >> graph[i][j];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				if (graph[j][i] && graph[i][k]) {
+					graph[j][k] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			std::cout << graph[i][j] << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	return 0;
+}
