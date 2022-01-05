7409a4f15a7e3698fa57e703911d7d8454523329
dohyun06 <hyun4you2006@naver.com>
2022-01-05 09:52:14 +0900

    Create BOJ 1107.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1107.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#define INF 10000000
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N, M, cnt = 0, sum[3] = { INF, INF, INF };
+	bool flag = false;
+	std::string sN;
+
+	std::cin >> N >> M;
+
+	sN = std::to_string(N);
+	std::vector<int> broken(M);
+
+	for (int i = 0; i < M; i++) std::cin >> broken[i];
+
+	sum[0] = std::abs(N - 100);
+	if (!M) sum[1] = sN.length();
+
+	for (int i = 0; i < M; i++)
+		if (sN.find((char)(broken[i] + '0')) != std::string::npos) break;
+		else if (i == M - 1) sum[1] = sN.length();
+
+	if (sum[1] == INF) {
+		for (int i = 1; i <= 1000000; i++) {
+			cnt++;
+
+			if (N - i >= 0) {
+				sN = std::to_string(N - i);
+
+				for (int j = 0; j < M; j++) {
+					if (sN.find((char)(broken[j] + '0')) != std::string::npos) break;
+					else if (j == M - 1) {
+						sum[2] = sN.length() + cnt;
+						flag = true;
+						break;
+					}
+				}
+			}
+			if (flag) break;
+
+			sN = std::to_string(N + i);
+
+			for (int j = 0; j < M; j++) {
+				if (sN.find((char)(broken[j] + '0')) != std::string::npos) break;
+				else if (j == M - 1) {
+					sum[2] = sN.length() + cnt;
+					flag = true;
+					break;
+				}
+			}
+
+			if (flag) break;
+		}
+	}
+
+	std::cout << *std::min_element(sum, sum + 3);
+
+	return 0;
+}
