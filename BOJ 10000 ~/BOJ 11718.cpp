010bf8662c4e9c87458b99aebe5e409b7e6a87ab
dohyun06 <hyun4you2006@naver.com>
2022-01-09 10:05:09 +0900

    Create BOJ 11718.cpp

---
+++ BOJ 10000 ~/BOJ 11718.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main(void) {
+    std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+    std::string str;
+
+    while (std::getline(std::cin, str)) {
+        std::cout << str << '\n';
+    }
+
+    return 0;
+}
