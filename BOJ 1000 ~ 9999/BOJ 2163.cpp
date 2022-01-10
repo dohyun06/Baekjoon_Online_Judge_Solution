209b39763267b9b475a3292d2233223e5e5aa964
dohyun06 <hyun4you2006@naver.com>
2022-01-10 11:15:11 +0900

    Create BOJ 2163.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 2163.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>;
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N, M;
+
+	std::cin >> N >> M;
+	std::cout << N * M - 1;
+
+	return 0;
+}
