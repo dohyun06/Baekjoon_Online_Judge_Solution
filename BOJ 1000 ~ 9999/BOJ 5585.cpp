29ac3153963b2793656e172da635a60ac05536e5
dohyun06 <hyun4you2006@naver.com>
2022-01-11 22:30:15 +0900

    Create BOJ 5585.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 5585.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int input, left, coin[6] = { 500,100,50,10,5,1 }, sum = 0;
+
+	std::cin >> input;
+
+	left = 1000 - input;
+
+	for (int i = 0; i < 6; i++) {
+		sum += left / coin[i];
+		left %= coin[i];
+	}
+
+	std::cout << sum;
+
+	return 0;
+}
