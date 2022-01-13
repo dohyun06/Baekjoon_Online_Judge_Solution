24469b8a98cddb74a1159d30665e82d1b0a99f42
dohyun06 <hyun4you2006@naver.com>
2022-01-13 10:21:28 +0900

    Create BOJ 11053.cpp

---
+++ BOJ 10000 ~/BOJ 11053.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N;
+
+	std::cin >> N;
+
+	std::vector<int> A(N), dp(N, 1);
+
+	for (int i = 0; i < N; i++) std::cin >> A[i];
+
+	for (int i = 0; i < N; i++)
+		for (int j = i; j < N; j++)
+			if (A[i] < A[j] && dp[j] <= dp[i])
+				dp[j] = dp[i] + 1;
+
+	std::cout << *std::max_element(dp.begin(), dp.end());
+
+	return 0;
+}
