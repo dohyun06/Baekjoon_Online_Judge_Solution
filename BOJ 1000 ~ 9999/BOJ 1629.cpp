c4750932003534006ea633d7697155b4f3d742e3
dohyun06 <hyun4you2006@naver.com>
2022-01-13 11:27:01 +0900

    Create BOJ 1629.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1629.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+long long A, B, C;
+
+long long pow(long long A, long long B) {
+    if (B == 0) return 1;
+    if (B == 1) return A % C;
+
+    long long res = pow(A, B / 2) % C;
+    
+    return (((res % C) * (res % C)) % C) * (B % 2 ? A % C : 1);
+}
+
+int main(void) {
+    std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+    std::cin >> A >> B >> C;
+
+    std::cout << pow(A, B) % C;
+
+    return 0;
+}
