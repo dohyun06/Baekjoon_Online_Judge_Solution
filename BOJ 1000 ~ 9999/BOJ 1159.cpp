393e2e800a429d26bc26153d0e083a0761417657
dohyun06 <hyun4you2006@naver.com>
2022-01-15 20:05:02 +0900

    Create BOJ 1159.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1159.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int N, alph[26] = { 0, };
+	std::string input;
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> input;
+		alph[input[0] - 'a']++;
+	}
+
+	if (*std::max_element(alph, alph + 26) < 5)std::cout << "PREDAJA";
+	else
+		for (int i = 0; i < 26; i++)
+			if (alph[i] >= 5) std::cout << (char)(i + 'a');
+
+	return 0;
+}
