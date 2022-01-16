4522b3dd17783672cc06cb17e81dda232498d3cc
dohyun06 <hyun4you2006@naver.com>
2022-01-16 09:12:24 +0900

    Create BOJ 24228.cpp

---
+++ BOJ 10000 ~/BOJ 24228.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	long long N, R;
+	std::cin >> N >> R;
+	std::cout << N + 2 * R - 1;
+
+	return 0;
+}
