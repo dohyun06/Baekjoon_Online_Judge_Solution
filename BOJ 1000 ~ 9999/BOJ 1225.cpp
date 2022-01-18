6a52570ec91fab53808a47eb8aea1ba5eae67f5e
dohyun06 <hyun4you2006@naver.com>
2022-01-18 23:40:25 +0900

    Create BOJ 1225.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1225.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	long long sum = 0;
+	std::string A, B;
+
+	std::cin >> A >> B;
+
+	for (int i = 0; i < A.length(); i++)
+		for (int j = 0; j < B.length(); j++)
+			sum += (A[i] - '0') * (B[j] - '0');
+
+	std::cout << sum;
+
+	return 0;
+}
