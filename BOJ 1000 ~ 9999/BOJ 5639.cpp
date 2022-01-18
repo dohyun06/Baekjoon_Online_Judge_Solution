71f6e8f50b0b802c7c34e73b57d34031e1d675b4
dohyun06 <hyun4you2006@naver.com>
2022-01-18 23:40:27 +0900

    Create BOJ 5639.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 5639.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<int> node;
+
+void preToPost(int l, int r) {
+	int sub = l + 1;
+	while (sub <= r && node[sub] < node[l]) sub++;
+
+	if (l + 1 <= sub - 1) preToPost(l + 1, sub - 1);
+	if (sub <= r) preToPost(sub, r);
+
+	std::cout << node[l] << '\n';
+}
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int	input;
+	
+	while (std::cin >> input) node.push_back(input);
+
+	preToPost(0, node.size() - 1);
+
+	return 0;
+}
