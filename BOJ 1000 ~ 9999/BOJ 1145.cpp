d923039993c5a5482b1208f48094cacc7c2ef854
dohyun06 <hyun4you2006@naver.com>
2022-01-19 23:29:40 +0900

    Create BOJ 1145.cpp

---
+++ BOJ 1000 ~ 9999/BOJ 1145.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+	int arr[5],cnt=0;
+
+	for (int i = 0; i < 5; i++) std::cin >> arr[i];
+
+	for (int i = 1;; i++) {
+		for (int j = 0; j < 5; j++)
+			if (!(i % arr[j])) cnt++;
+
+		if (cnt >= 3) {
+			std::cout << i;
+			break;
+		}
+
+		cnt = 0;
+	}
+
+	return 0;
+}
