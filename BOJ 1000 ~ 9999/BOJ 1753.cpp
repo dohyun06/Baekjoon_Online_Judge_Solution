a5300fe2f4b20179d52bb7312b8a27606352b304
dohyun06 <hyun4you2006@naver.com>
2023-01-15 21:45:27 +0900

    .

---
+++ BOJ 1000 ~ 9999/BOJ 1753.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define INF 20
+
+int V, E, K;
+std::vector<int> dist;
+std::vector<std::vector<std::pair<int, int>>> path;
+
+void dijkstra(){
+	std::priority_queue<std::pair<int, int>> pq;
+
+	pq.push(std::make_pair(0, K));
+	dist[K] = 0;
+
+	while (!pq.empty()) {
+		int cost = -pq.top().first;
+		int cur = pq.top().second;
+		pq.pop();
+
+		if (dist[cur] < cost) continue;
+
+		for (int i = 0; i < path[cur].size(); i++){
+			int next = path[cur][i].first, nextCost = cost + path[cur][i].second;
+			
+			if (dist[next] > nextCost) {
+				dist[next] = nextCost;
+				pq.push(std::make_pair(-nextCost, next));
+			}
+		}
+	}
+}
+
+int main(void) {
+	std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+  std::cin >> V >> E >> K;
+
+	path.resize(V + 1);
+	dist.resize(V + 1, INF);
+
+  int u, v, w;
+
+	for (int i = 0; i < E; i++) {
+		std::cin >> u >> v >> w;
+		path[u].push_back({ v, w });
+	}
+
+	dijkstra();
+
+  for (int i = 0; i < dist.size(); i++)
+    if (dist[i] < 1000000) std::cout << dist[i] << '\n';
+    else std::cout << "INF" << '\n';
+
+	return 0;
+}
