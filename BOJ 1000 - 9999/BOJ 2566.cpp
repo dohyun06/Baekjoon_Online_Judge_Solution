fa73c7fe88a3eac87a3757efd99d3db3e24b5be4
Dohyun Kim <hyun4you2006@naver.com>
2025-05-29 21:11:24 +0900

    BOJ 2566

---
+++ BOJ 1000 - 9999/BOJ 2566.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main(void) {
+    std::cout.tie(NULL); std::cin.tie(NULL); std::ios_base::sync_with_stdio(false);
+
+    int max = 0, line = 1, row = 1, input;
+
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            std::cin >> input;
+
+            if(input > max) {
+                max = input;
+                line = i + 1;
+                row = j + 1;
+            }
+        }
+    }
+
+    std::cout << max << '\n' << line << ' ' << row;
+
+    return 0;
+}
